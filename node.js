7a105decd003c7e8102b5487561dd09e30d43299
Cadertea <cadertea@ncsu.edu>
2020-11-23 10:31:02 -0500

    added functions to view tables that have been generated on sql, and command line

--- node.js
+++ node.js
@@ -24,9 +24,9 @@ function start() {
         name: "choice",
         choices: [
             "View All Employees?",
-            "View All Employee's By Roles?",
-            "View all Emplyees By Deparments",
-            "Update Employee",
+            "View All Employees By Roles?",
+            "View all Employees By Deparments?",
+            "Update Employee?",
             "Add Employee?",
             "Add Role?",
             "Add Department?"
@@ -64,4 +64,53 @@ function start() {
 
         }
     })
+}
+
+function viewAllEmployees (){
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      start()
+  })
+
+}
+
+function viewAllDepartments(){
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",, 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      start()
+  })
+}
+
+
+function viewAllRoles(){
+    connection.query("SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",, 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      start()
+  })
+}
+
+function addEmployee(){
+
+
+}
+
+function updateEmployee(){
+
+
+}
+
+function addRole() {
+
+
+}
+
+function addDepartment(){
+
+    
 }
